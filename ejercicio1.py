2d307acd76b6c34c1132fd1119a366aa6a952d27
Lkss <lucasdesiderio000@gmail.com>
2025-03-28 17:17:06 -0300

    Se agregaron comentarios a ejercicio1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,4 +1,7 @@
+# Se define variable nombre
 nombre = "Lkss"
+# Se define variable materia
 materia = "Programación I"
+# Se imprime el mensaje por consola
 
 print(f"Soy {nombre} y estoy cursando {materia}")
